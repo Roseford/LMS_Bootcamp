86c99247a1a60058f61404e783d5c4b299f872b8
Roseford <obianujuofodu@gmail.com>
2023-07-04 11:16:19 +0100

    added a print function

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -20,6 +20,7 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     # CREATE AND RETURN A TOKEN
     access_token = oauth2.create_access_token(data = {"user_id": user.id})
     
+    print(user_credentials.password, user_credentials.username)
     return {"access_token": access_token, "token_type": "bearer", "user": user}
 
 
